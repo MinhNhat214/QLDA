e8e193fd9b076ef9e1ca1ab4f74a787cb928bce5
MinhNhat214 <nhattran200411@gmail.com>
2024-06-16 17:28:50 -0700

    RoomTypeModel_Update

--- src/Model/RoomTypeModel.php
+++ src/Model/RoomTypeModel.php
@@ -11,6 +11,22 @@ class RoomTypeModel extends DBInteraction
             return [];
         }
     }
+    public function getRoomTypeInner()
+    {
+        $sql = "
+            SELECT rooms.room_id, rooms.room_number, room_types.base_price, room_types.type_name, room_types.room_type_id
+            FROM rooms
+            INNER JOIN room_types
+            ON rooms.room_type_id = room_types.room_type_id
+            GROUP BY rooms.room_type_id
+        ";
+        $getRaw = $this->getRaw($sql);
+        if ($getRaw) {
+            return $getRaw;
+        } else {
+            return [];
+        }
+    }
     // public function getTypeName()
     // {
     //     $getRaw = $this->oneRaw('SELECT * FROM room_types WHERE room');
@@ -21,5 +37,3 @@ class RoomTypeModel extends DBInteraction
     //     }
     // }
 }
-
-
